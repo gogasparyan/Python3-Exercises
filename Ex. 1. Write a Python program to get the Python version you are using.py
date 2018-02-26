1448673c1cd79ae82b562292b63951fbe1518541
Gor Gasparyan <gor_gasparyan@outlook.com>
2018-02-26 09:42:36 +0400

    001

---
+++ Ex. 1. Write a Python program to get the Python version you are using.py
@@ -0,0 +1,14 @@
+####################################################################################
+###   Required: write a Python program to get the Python version you are using   ###
+####################################################################################
+
+import sys 
+"""importing sys module which provides access 
+to some variables used or maintained by the 
+interpreter and to functions that interact 
+strongly with the interpreter.
+"""
+print("Python version:")			
+print(sys.version)
+print("\nVersion info:")
+print(sys.version_info)
