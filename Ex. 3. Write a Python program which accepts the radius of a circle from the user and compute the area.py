5be2b07501ad8f91343de2773d105c3aa8fd5d31
Gor Gasparyan <20324876+gogasparyan@users.noreply.github.com>
2018-03-22 03:42:17 +0400

    Ex. 3 - OK

--- Ex. 3. Write a Python program which accepts the radius of a circle from the user and compute the area.py
+++ Ex. 3. Write a Python program which accepts the radius of a circle from the user and compute the area.py
@@ -10,3 +10,26 @@
 ###																								####################
 ####################################################################################################################
 ####################################################################################################################
+
+from math import pi
+
+r = ""
+
+while r!=0:
+	try:
+		r = int(input("\nEnter radius of a cirlce:\t"))
+		if r<=0:
+			print("\nYou must enter positive integer")
+			continue
+		r = float(r)
+		s = pi*(r**2)
+
+		print("\nRadius of a circle is:\tr = " + str(r))
+		print("Area of a circle:\tS = " + str(s))
+		print("(Enter '0' for exit)")
+		
+	except (ValueError, TypeError):
+		print("\n\t*** Error occurred ***\n\n You must enter positive integer")
+		continue
+
+print("\nAdios!")
